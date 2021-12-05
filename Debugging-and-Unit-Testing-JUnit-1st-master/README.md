d1d983e46a91d6f00dbe09df1ee3174745d8f924
Javier <javierggjavier@gmail.com>
2021-12-05 15:25:53 +0000

    Created CHECKING instance variable and isChecking method in BankAccount class

---
+++ Debugging-and-Unit-Testing-JUnit-1st-master/README.md
@@ -0,0 +1,3 @@
+# Debugging-and-Unit-Testing-JUnit-1
+I wrote this code in Java 8 with the IDE Intellij. 
+This application uses the JUnit 4 API (Classes and Annotations), as well as the JUnit 4 jupiter API to check some tests. I used a secondary branch to practice how to work with more developers. I followed the instructions of the teacher Tim Buchalka (Udemy, Java Programming Masterclass covering Java 11 and Java 17).
---
+++ Debugging-and-Unit-Testing-JUnit-1st-master/src/com/javier/BankAccount.java
@@ -0,0 +1,44 @@
+package com.javier;
+
+
+public class BankAccount {
+
+    public static final Object CHECKING = 1;
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, double balance, Object o) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    // The branch argument is true if the customer is performing the transaction
+    // at a branch, with a teller.
+    // It's false if the customer is performing the transaction at an ATM
+    public double deposit(double amount, boolean branch) {
+        balance += amount;
+        return balance;
+    }
+
+    // The branch argument is true if the customer is performing the transaction
+    // at a branch, with a teller.
+    // It's false if the customer is performing the transaction at an ATM
+    public double withdraw(double amount, boolean branch) {
+        balance -= amount;
+        return balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean isChecking() {
+        System.out.println("This method is checking...");
+        return true;
+    }
+
+    // More methods that use firstName, lastName, and perform other functions
+
+}
